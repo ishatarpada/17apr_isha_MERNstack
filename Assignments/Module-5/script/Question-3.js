0fa5101d5498caab48e015b4f51a795415f8bb65
Isha Tarpada <123743446+ishatarpada@users.noreply.github.com>
2024-01-13 20:02:35 +0530

    Delete Assignments/Module-5 directory

--- Assignments/Module-5/script/Question-3.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function () {
-    $('#hero').cycle('shuffle','scrollRight');
-});
--- Assignments/Module-5/script/Question-4.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('span').parents().css({
-        'border': '3px solid red',
-        'color': 'red'
-    })
-});
