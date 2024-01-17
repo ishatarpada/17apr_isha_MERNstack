79a30f2dd8f990bfc1ce65acb2d63d4212c6f85d
Isha Tarpada <123743446+ishatarpada@users.noreply.github.com>
2024-01-17 10:50:15 +0530

    Add files via upload

--- Assignments/Module-5/script/Question-3.js
+++ Assignments/Module-5/script/Question-3.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-    $('#hero').cycle('shuffle','scrollRight');
+  function animateBox() {
+    $("#animatedBox").animate({
+      left: '250px',
+      opacity: '0.5',
+      height: '250px',
+      width: '250px'
+    }, 2000, function () {
+    });
+  }
+
+  animateBox();
 });
