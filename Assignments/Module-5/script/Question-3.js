74ad8d12c1914a1b68465d42d9254f9f0b7b8de0
Isha Tarpada <123743446+ishatarpada@users.noreply.github.com>
2024-01-26 11:13:05 +0530

    Add files via upload

--- Assignments/Module-5/script/Question-3.js
+++ Assignments/Module-5/script/Question-3.js
@@ -1,13 +1,38 @@
 $(document).ready(function () {
-  function animateBox() {
-    $("#animatedBox").animate({
-      left: '250px',
-      opacity: '0.5',
-      height: '250px',
-      width: '250px'
-    }, 2000, function () {
-    });
-  }
+  $("#animateBtn").click(function () {
+    // create the variable
+    var slider = $("#slider-container");
 
-  animateBox();
+    // create the array for images.
+    var images = [
+      'url("https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630")',
+      'url("https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg")',
+      'url("https://i0.wp.com/picjumbo.com/wp-content/uploads/amazing-sunset-mountain-scenery-with-inversion-free-photo.jpg?w=600&quality=80")',
+      'url("https://deep-image.ai/blog/content/images/2022/09/underwater-magic-world-8tyxt9yz.jpeg")',
+    ];
+
+    //  index for track the image.
+    var index = 0;
+
+    // Function to change the background image with fade effect
+    function changeImage() {
+      slider.animate({ opacity: 0 }, "slow", function () {
+        slider.css("background-image", images[index]);
+        slider.animate({ opacity: 1 }, "slow");
+      });
+
+      index = (index + 1) % images.length;
+    }
+
+    // call the function for change image
+    changeImage();
+
+    // Set interval to change image every 5 seconds
+    var interval = setInterval(changeImage, 5000);
+
+    // // Stop the interval when we click the image
+    $("#animateBtn").click(function () {
+      clearInterval(interval);
+    });
+  });
 });
