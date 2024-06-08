7104d4384d199074e481661ba5b197c8044be6e1
Isha Tarpada <123743446+ishatarpada@users.noreply.github.com>
2024-06-08 21:57:23 +0530

    Add files via upload

---
+++ Assignments/React-js/Module-1/registration-form/src/Form.jsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+import FormImage from "./FormImage"
+import FormContent from "./FormContent"
+import './index.css'
+
+
+export default function Form() {
+  return (
+    <>
+      <div className="box-area">
+        <FormImage></FormImage>
+        <FormContent></FormContent>
+      </div>
+    </>
+  )
+}
