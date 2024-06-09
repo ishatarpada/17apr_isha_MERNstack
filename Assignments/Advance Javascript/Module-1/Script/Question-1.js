617bceec58f2a87fcc9b954273b35f8692ccfdf7
Isha Tarpada <123743446+ishatarpada@users.noreply.github.com>
2024-06-09 14:09:45 +0530

    Delete Assignments/Advance Javascript directory

--- Assignments/Advance Javascript/Module-1/Script/Question-1.js
+++
@@ -1,8 +0,0 @@
-// Get the button element by its ID
-const btn = document.getElementById("btn");
-
-// Add a click event listener to the button
-btn.addEventListener("click", function () {
-  // Display an alert when the button is clicked
-  alert("this is alert box");
-});
--- Assignments/Advance Javascript/Module-1/Script/Question-4.js
+++
@@ -1,8 +0,0 @@
-// Get the button element by its ID
-const btn = document.getElementById("btn");
-
-// Add a click event listener to the button
-btn.addEventListener("click", function () {
-  // Alert the result of the given code
-  alert( null || 2 || undefined);
-});
